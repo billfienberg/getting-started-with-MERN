4a2ded8c2aa7a994866844d3e5731c31bbcc59b2
Bill Fienberg <wfienberg2@hotmail.com>
2017-01-07 10:50:21 -0600

    Use axios to fetch comment data from API

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,5 +1,6 @@
 //CommentBox.js
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import style from '../style';
@@ -8,6 +9,24 @@ class CommentBox extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  loadCommentsFromServer() {
+    axios.get(this.props.url)
+    .then(res => {
+      this.setState({ data: res.data });
+    })
+  }
+
+  handleCommentSubmit(comment) {
+    //add POST request
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   }
 
   render() {
@@ -15,7 +34,7 @@ class CommentBox extends Component {
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
         <CommentList data={ this.state.data }/>
-        <CommentForm />
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit } />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CommentBox from './CommentBox';
 
 ReactDOM.render(
   <CommentBox
-    url='http:localhost:3001/api/comments'
+    url='http://localhost:3001/api/comments'
     pollInterval={2000}
   />,
   document.getElementById('root')
