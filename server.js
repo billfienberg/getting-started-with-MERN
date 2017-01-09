6c160fcbc202e81ad2586c8d25f9900798d873a2
Bill Fienberg <wfienberg2@hotmail.com>
2017-01-09 07:00:36 -0600

    Add GET/PUT/DELETE for comments/:id to server.js

--- server.js
+++ server.js
@@ -66,6 +66,52 @@ router.route('/comments')
   });
 });
 
+//Adding a route to a specific comment based on the database ID
+router.route('/comments/:comment_id')
+  .get(function(req, res) {
+    const commentId = req.params.comment_id;
+    Comment.findById(commentId, function(err, comment) {
+      if (err) {
+        res.send(err);
+      }
+      console.log("comment: ", comment)
+      // return comment;
+      res.json(comment)
+    })
+  })
+  //The put method gives us the chance to update our comment based on the ID passed to the route
+  .put(function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+      if (err) {
+        res.send(err);
+      }
+
+      //setting the new author and text to whatever was changed. If nothing was changed
+      // we will not alter the field.
+      (req.body.author) ? comment.author = req.body.author : null;
+      (req.body.text) ? comment.text = req.body.text : null;
+
+      //save comment
+      comment.save(function(err) {
+
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: 'Comment has been updated' });
+      });
+    });
+  })
+  //delete method for removing a comment from our database
+  .delete(function(req, res) {
+    //selects the comment by its ID, then removes it.
+    Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Comment has been deleted' })
+    })
+  });
+
 // Use our router configuration when we call /api
 app.use('/api', router);
 
